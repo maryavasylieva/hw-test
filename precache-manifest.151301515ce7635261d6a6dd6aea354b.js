9c0c0c64600d8569494d5386e73d48879bc8a9c1
maryavasylieva <miheta.laurelin@gmail.com>
2019-07-12 11:53:39 +0300

    Updates

---
+++ precache-manifest.151301515ce7635261d6a6dd6aea354b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b3f2127b666781fe2dcdaa8a908874",
+    "url": "/hw-test/./index.html"
+  },
+  {
+    "revision": "67d0dded3a54f683f9eb",
+    "url": "/hw-test/./static/css/main.7fd0b74d.chunk.css"
+  },
+  {
+    "revision": "33976dc82018f1516642",
+    "url": "/hw-test/./static/js/2.b2c71802.chunk.js"
+  },
+  {
+    "revision": "67d0dded3a54f683f9eb",
+    "url": "/hw-test/./static/js/main.bf7d14ef.chunk.js"
+  },
+  {
+    "revision": "7fbebf0fa8c5ec335025",
+    "url": "/hw-test/./static/js/runtime~main.5a6fcf0d.js"
+  }
+]);
